0a227d24022db3e65e4e3ccb2467c3dac45986a3
Mohsin <101992170+MohsinSolangi@users.noreply.github.com>
2026-08-19 20:29:04 +0500

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+int buyprice= prices[0];
+int maxprofit = 0;
+for(int i =1 ; i<prices.length; i++){
+    if(buyprice>prices[i]){
+        buyprice=prices[i] ; }
+
+    maxprofit= Math.max(maxprofit, prices[i] - buyprice);
+}
+return maxprofit;
+   }
+   
+}
